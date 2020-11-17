9cfbee725e06782c491f2ab277000d2485a242d6
Willamys Araujo <willamysaraujo7@gmail.com>
2020-11-17 17:44:59 -0300

    Nova funcionalidade

--- git-workflow-eclipse/src/HelloMain.java
+++ git-workflow-eclipse/src/HelloMain.java
@@ -2,6 +2,6 @@ import javax.swing.JOptionPane;
 
 public class HelloMain {
 	public static void main(String[] args) {
-		JOptionPane.showMessageDialog(null, "Hello");
+		JOptionPane.showMessageDialog(null, "Hello Funcionalidade X");
 	}
 }
