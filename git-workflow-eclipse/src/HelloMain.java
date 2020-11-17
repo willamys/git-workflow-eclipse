ac0c341b2c117fce2709eb76aaaae17ee0475389
Willamys Araujo <willamysaraujo7@gmail.com>
2020-11-17 19:40:53 -0300

    ..

--- git-workflow-eclipse/src/HelloMain.java
+++ git-workflow-eclipse/src/HelloMain.java
@@ -2,10 +2,6 @@ import javax.swing.JOptionPane;
 
 public class HelloMain {
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		JOptionPane.showMessageDialog(null, "Hello Helo World");
-=======
 		JOptionPane.showMessageDialog(null, "Hello Funcionalidade X - modificado na net");
->>>>>>> refs/remotes/origin/funcionalidade_x
 	}
 }
